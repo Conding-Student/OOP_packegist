7583c825faf039aae5c56669142c054d1b2d6a72
Ronnie Valenzuela <onie.valenzuelaaa02@gmail.com>
2024-04-17 20:50:13 +0800

    title

--- index.php
+++ index.php
@@ -21,13 +21,23 @@ else{
 }
 ?>
 <!DOCTYPE html>
+
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+    <head>
     
-</body>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <title>Sentiment Analyzer</title>
+        <link rel="stylesheet" href="style.css">
+    
+    </head>
+    
+    <body>
+    
+        
+        
+    </body>
+
 </html>
